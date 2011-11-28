1ec58a3d11c20ebbcbb986e8b6835758f8520555
Hans E Hyttinen <hans@lumilux.org>
2011-11-28 02:28:14 -0500

    fixed ArrayIndexOutOfBounds

--- src/mirroruniverse/g6/G6Player.java
+++ src/mirroruniverse/g6/G6Player.java
@@ -116,7 +116,7 @@ public class G6Player implements Player {
 		*/
 	}
 	
-	@Override
+	//@Override
 	public int lookAndMove(int[][] leftView, int[][] rightView) {
 		updateKnowledge(left, x1, y1, leftView);
 		updateKnowledge(right, x2, y2, rightView);
@@ -219,7 +219,7 @@ public class G6Player implements Player {
 				}
 			}
 		}
-		if(solution != null) {
+		if(solution != null && solutionStep < solution.length) {
 			return solution[solutionStep++];
 		}
 		
