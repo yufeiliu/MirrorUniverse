8b73129ceb0b666475688bf184c050f756a03d6f
Sid Nair <ssn2114@columbia.edu>
2011-11-30 00:03:49 -0500

    update simulator

--- src/mirroruniverse/sim/MirrorUniverse.java
+++ src/mirroruniverse/sim/MirrorUniverse.java
@@ -98,6 +98,7 @@ public class MirrorUniverse
 			int[][] aintViewR = mumMapR.getView();
 			int intMove = plrCurrent.lookAndMove( aintViewL, aintViewR );
 			try {
+				System.out.println( intStep + ":::::" );
 				if ( intStep != 0 )
 				{
 					bfrReplay.write( ", " );
@@ -117,9 +118,6 @@ public class MirrorUniverse
 		int intStepMax = intStepL > intStepR ? intStepL : intStepR;
 		int intStepMin = intStepL > intStepR ? intStepR : intStepL;
 		int intStepDiff = intStepMax - intStepMin;
-		System.out.println("===============");
-		System.out.println("Diff: " + intStepDiff);
-		System.out.println("Steps: " + intStepMax);
 		if ( intStepMax != intStep )
 			System.out.println( "Step inconsistent" );
 		
